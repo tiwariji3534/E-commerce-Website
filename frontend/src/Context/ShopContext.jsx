ea3c81574901a853c57211f6db6d3819ab2466ed
tiwariji3534 <145870342+tiwariji3534@users.noreply.github.com>
2024-07-02 03:37:44 +0530

    Update ShopContext.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -19,12 +19,12 @@ const ShopContextProvider = (props) =>{
     const[cartItems,setCartItems] = useState(getDefaultCart());
 
     useEffect(()=>{
-        fetch('https://e-commerce-website-1-hp76.onrender.com/allproduct')
+        fetch('https://e-commerce-website-tym5.onrender.com/allproduct')
         .then((response)=>response.json())
         .then((data)=>setAll_Product(data))
 
         if(localStorage.getItem('auth-token')){
-            fetch('https://e-commerce-website-1-hp76.onrender.com/getcart',{
+            fetch('https://e-commerce-website-tym5.onrender.com/getcart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -42,7 +42,7 @@ const ShopContextProvider = (props) =>{
     const addToCart = (itemId)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         if(localStorage.getItem('auth-token')){
-            fetch('https://e-commerce-website-1-hp76.onrender.com/addtocart',{
+            fetch('https://e-commerce-website-tym5.onrender.com/addtocart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -59,7 +59,7 @@ const ShopContextProvider = (props) =>{
     const removeFromCart = (itemId)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
         if(localStorage.getItem('auth-token')){
-            fetch('https://e-commerce-website-1-hp76.onrender.com/removefromcart',{
+            fetch('https://e-commerce-website-tym5.onrender.com/removefromcart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
