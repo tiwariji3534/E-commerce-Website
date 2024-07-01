5b2dca794ccbf6503d41e3c8a0b8aefe6d172335
tiwariji3534 <145870342+tiwariji3534@users.noreply.github.com>
2024-07-01 15:39:01 +0530

    Update AddProduct.jsx 37 49

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -34,7 +34,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product',image);
         
-        await fetch('http://localhost:4000/upload',{
+        await fetch('/upload',{
           method: 'POST',
           headers:{
             Accept:'application/json',
@@ -46,7 +46,7 @@ const AddProduct = () => {
         {
           product.image = responseData.image_url;
           console.log(product);
-          await fetch('http://localhost:4000/addproduct',{
+          await fetch('/addproduct',{
             method:'POST',
             headers:{
               Accept:'application/json',
