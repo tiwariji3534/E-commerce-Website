96cb308e611ff032427f6ac253d7bf0a2f0cc014
tiwariji3534 <145870342+tiwariji3534@users.noreply.github.com>
2024-07-02 22:57:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ const path = require("path");
 const cors = require("cors");
 
 app.use(express.json());    // jo request milega wo json k through pass hoga
-app.use(cors());          //  react js project connect to express on port 4000
+// app.use(cors());          //  react js project connect to express on port 4000
+app.use(cors({
+    origin: "*", // Set this to your frontend URL if applicable
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 
 
 // DATABASE CONNECTION WITH MONGODB
