9d278feeff1ef1e2d7e345a2b1052e7aa6162667
tiwariji3534 <145870342+tiwariji3534@users.noreply.github.com>
2024-07-03 13:03:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const path = require("path");
 const cors = require("cors");
 
 app.use(express.json());    // jo request milega wo json k through pass hoga
-app.use(cors(https://e-commerce-website-2-d2hp.onrender.com));          //  react js project connect to express on port 4000
+app.use(cors(https://e-commerce-website-1-admin.onrender.com));          //  react js project connect to express on port 4000
 // app.use(cors({
 //     origin: "*", // Set this to your frontend URL if applicable
 //     methods: ["POST", "GET"],
